33832a6a1d987324ca72c14f7675f3f06a6275f6
ujjwal <ujjwal@192.168.1.6>
2020-08-17 23:11:02 +0530

    commit : Add chuck Norris class

--- src/main/java/com/jokes/jokesApp/controller/JokeController.java
+++ src/main/java/com/jokes/jokesApp/controller/JokeController.java
@@ -1,17 +1,21 @@
 package com.jokes.jokesApp.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.jokes.jokesApp.service.ChuckNorrisJokeService;
 
-@Controller
+@RestController
 public class JokeController {
 
 	ChuckNorrisJokeService chuckNorrisJokeService;
 	
-	@RequestMapping(value = "/get", method = RequestMethod.GET)
+	JokeController(ChuckNorrisJokeService chuckNorrisJokeService){
+		this.chuckNorrisJokeService = chuckNorrisJokeService;
+	}
+	
+	@RequestMapping(value = "/getJoke", method = RequestMethod.GET)
 	public String getJoke() {
 		return chuckNorrisJokeService.getJoke();
 	}
